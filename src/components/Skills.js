5013891141aec152449de4c629c424f926031b9b
Joseph Kessler <kesslerjx@icloud.com>
2022-06-27 12:10:31 -0400

    Add buttons to skill elements
    Skills can be deleted and moved up and down now. At the moment, it has
    basic buttons. I would really like to make them drag and drop, but I'm
    not worried about that at the moment. I'll add some style and icons to
    the skill buttons next.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,6 +3,11 @@ import '../styles/Info.css'
 import '../styles/Skills.css'
 import { v4 as uuidv4 } from 'uuid';
 
+const Direction = {
+    Up: 'u',
+    Down: 'd'
+}
+
 class Skills extends React.Component {
 
     constructor(props) {
@@ -43,8 +48,44 @@ class Skills extends React.Component {
 
     getSkillDiv(skill) {
         return (
-            <div key={uuidv4()}>{skill}</div>
+            <div key={uuidv4()} className='skill-wrapper'>
+                <p>{skill}</p>
+                <div>
+                    <button onClick={(e) => this.moveSkill(e, Direction.Up)}>U</button>
+                    <button onClick={(e) => this.moveSkill(e, Direction.Down)}>D</button>
+                    <button onClick={(e) => this.removeSkill(e)}>X</button>
+                </div>
+            </div>
         )
+    }
+
+    moveSkill(event, direction) {
+
+        let index    = this.getSkillIndex(event);
+        let moveable = (direction === Direction.Up ? index > 0 : index < this.props.resume.skills.length);
+
+        if(moveable) {
+            let skill = this.props.resume.skills[index];
+            let copy  = this.props.resume;
+
+            copy.skills.splice(index, 1);
+            copy.skills.splice(direction === Direction.Up ? index-1 : index+1, 0, skill);
+
+            this.props.updateResume(copy);
+        }
+    }
+
+    removeSkill(event) {
+        let index = this.getSkillIndex(event);
+        let copy  = this.props.resume;
+        copy.skills.splice(index, 1);
+        this.props.updateResume(copy);
+    }
+
+    getSkillIndex(event) {
+        let skill  = event.target.parentElement.parentElement;
+        let nodes  = Array.from(document.querySelector('.skills-wrapper').childNodes);
+        return nodes.indexOf(skill);
     }
 }
 
